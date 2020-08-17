5ad3337e5ad6ee0a6a52b482dbdebce0c163b632
javi-mera <fcojmera@gmail.com>
2020-08-17 10:54:58 +0000

    finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,32 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector("#a");
+  card = addEventListener("click", function() {
+    let cd = ["&spades;", "&clubs;", "&hearts;", "&diams;"];
+    let cho = Math.floor(Math.random() * 4);
+
+    document.querySelector("#b").innerHTML = cd[cho];
+
+    document.querySelector("#d").innerHTML = cd[cho];
+
+    let num = [
+      "A",
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "J",
+      "Q",
+      "K"
+    ];
+    let cho1 = Math.floor(Math.random() * 13);
+
+    document.querySelector("#c").innerHTML = num[cho1];
+  });
 };
